c4ce0b9a29e73f2e66ef631ae9afde87bb2c1a24
Adhokshaja_Babalad <addubablad26@gmail.com>
2025-01-29 15:25:16 +0530

    adding setup to code

--- code.cpp
+++ code.cpp
@@ -3,3 +3,13 @@ int green = 7;
 int blue = 6;
 int orange =5;
 int red =4;
+
+void setup()
+{
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(orange, OUTPUT);
+  pinMode(red, OUTPUT);
+  //pinMode(gas, INPUT);
+  Serial.begin(9600);
+}
